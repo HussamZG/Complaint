1ceaefe770b3106050aca9d799365c4fbd927dc7
HussamZG <hussamalzogher7@gmail.com>
2025-03-05 14:56:50 +0300

    Remove console logs for Supabase URL and Anon Key

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -3,11 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Anon Key:', supabaseAnonKey);
-
 if (!supabaseUrl || !supabaseAnonKey) {
-    throw new Error('Supabase URL and Anon Key are required.');
+    throw new Error('Supabase URL and Anon Key are required.')
 }
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
